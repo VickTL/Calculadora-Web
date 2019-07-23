0601560bfe005454b75cf0ab1bb7be47337ff41a
VickTL <juan1@nosfe.es>
2019-07-23 16:37:05 +0200

    Cambios en las fuentes y en js

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,12 +1,9 @@
 // Dados los datos ALTURA, MARGENES, FILAS e INTERLINEADO
-/*
  var altura = 0;
  var margenes = 0;
  var interlineado = 0;
  var filas = 0;
- bool masMenos = false;
-
-*/
+ var masMenos = false;
 
 // Medidas estandar en puntos
 const a5hor = 419.528;
@@ -29,15 +26,11 @@ var alturaCustom = 0;
 
 
 function pxtopt(alturaCustom) {
-    alturaCustom = alturaCustom*0.75;
-
-    //return alturaCustom
+    return alturaCustom*0.75;
 }
 
 function mmtopt(alturaCustom) {
-    alturaCustom = alturaCustom*2.835;
-
-    //return alturaCustom
+    return alturaCustom*2.835;
 }
 
 function calcLineasActuales(altura, margenes, interlineado) {
@@ -45,14 +38,12 @@ function calcLineasActuales(altura, margenes, interlineado) {
     var lineasActual = 0;
 
     lineasActual = altura-(2*margenes);
-    lineasActual = lineasActual/interlineado;
-
-    //return lineasActual; DUDA ¿Cómo puede la función calcLineasIdeales tener el valor de lineasActual si se declara en esta funcion?
+    return lineasActual/interlineado;
 }
 
 function calcLineasIdeales(masMenos, lineasActual, filas) {
     masMenos = masMenos; filas = filas;
-    var lineasIdeal = (int)lineasActual; // DUDA ¿Cómo cojo la parte entera solo de un numero?
+    var lineasIdeal = floor(lineasActual);
 
     // Esta funcion suma o resta lineas a las actuales hasta que de un numero entero divisible por el numero
     // de divisiones de la estructura (filas), es decir, hasta que al dividirlo el resto de 0.
@@ -69,5 +60,5 @@ function calcLineasIdeales(masMenos, lineasActual, filas) {
         } while((lineasIdeal-(filas-1))%filas != 0);
     }
 
-    // return lineasIdeal;
+    return lineasIdeal;
 }
