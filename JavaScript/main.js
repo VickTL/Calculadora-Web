7f28a2fcab7ee0f4dc7a0b4394018f5a9672f803
VickTL <juan1@nosfe.es>
2019-07-25 03:40:51 +0200

    live preview

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -6,6 +6,7 @@
  var filas = 0;
  var masMenos = false;
 
+
 // Medidas estandar en puntos
 const a5hor = 419.528;
 const a5ver = 595.276;
@@ -32,17 +33,22 @@ function mmtopt(alturaCustom) {
     return alturaCustom*2.835;
 }
 
-function calcLineasActuales(altura, margenes, interlineado) {
-    altura = altura; margenes = margenes; interlineado = interlineado;
+function calcLineasActuales() {
     var lineasActual = 0;
+    var medidaMargenes = 0;
+    document.interlineado = document.getElementById("interlineado").value;
+    document.filas = document.getElementById("divisiones").value;
 
-    lineasActual = altura-(2*margenes);
-    return lineasActual/interlineado;
+    medidaMargenes = document.altura-(2*document.margenes);
+    lineasActual = medidaMargenes/document.interlineado;
+
+    //console.log(lineasActual);
+    return lineasActual;
 }
 
 function calcLineasIdeales(masMenos, lineasActual, filas) {
     masMenos = masMenos; filas = filas;
-    var lineasIdeal = floor(lineasActual);
+    var lineasIdeal = Math.floor(lineasActual);
 
     // Esta funcion suma o resta lineas a las actuales hasta que de un numero entero divisible por el numero
     // de divisiones de la estructura (filas), es decir, hasta que al dividirlo el resto de 0.
@@ -84,43 +90,54 @@ function showHide2() {
 }
 
 function medidas() {
+
+    document.margenes = document.getElementById("margenes").value;
+
     if(document.getElementById("Vbtn").checked) {
         if (document.getElementById("A5btn").checked) {
             document.altura = a5ver;
             document.anchura = a5hor;
+            rectangulo();
         } else if(document.getElementById("A4btn").checked) {
             document.altura = a4ver;
             document.anchura = a4hor;
+            rectangulo();
         } else if(document.getElementById("A3btn").checked) {
             document.altura = a3ver;
             document.anchura = a3hor;
+            rectangulo();
         } else if (document.getElementById("HDbtn").checked) {
             document.altura = hdver;
             document.anchura = hdhor;
+            rectangulo();
         }
     } else if (document.getElementById("Hbtn").checked) {
         if (document.getElementById("A5btn").checked) {
             document.altura = a5hor;
             document.anchura = a5ver;
+            rectangulo();
         } else if(document.getElementById("A4btn").checked) {
             document.altura = a4hor;
             document.anchura = a4ver;
+            rectangulo();
         } else if(document.getElementById("A3btn").checked) {
             document.altura = a3hor;
             document.anchura = a3ver;
+            rectangulo();
         } else if (document.getElementById("HDbtn").checked) {
             document.altura = hdhor;
             document.anchura = hdver;
+            rectangulo();
         }
     } else if (document.getElementById("Othbtn").checked) {
         document.altura = document.getElementById("CustomV").value;
         document.anchura = document.getElementById("CustomH").value;
+        rectangulo();
     }
 
-    console.log("Altura: "+document.altura);
-    console.log("Anchura: "+document.anchura);
+    //console.log("Altura: "+document.altura);
+    //console.log("Anchura: "+document.anchura);
 
-    rectangulo();
 }
 
 function switchValues() {
@@ -134,13 +151,85 @@ function switchValues() {
 }
 
 function rectangulo() {
+    while(document.anchura>0.8*pxtopt(document.getElementById("mediapantalla").clientWidth) || document.altura>0.8*pxtopt(document.getElementById("mediapantalla").clientHeight)){
+        document.anchura=document.anchura*0.99;
+        document.altura=document.altura*0.99;
+
+        document.margenes=document.margenes*0.99;
+
+        //console.log("Altura: "+document.altura+", Anchura: "+document.anchura+", Margenes: "+document.margenes);
+    }
+
+
     var x = document.getElementById("canvas");
 
-    x.style.width = document.anchura+"px";
-    x.style.height = document.altura+"px";
+    x.style.width = document.anchura+"pt";
+    x.style.height = document.altura+"pt";
+    x.style.opacity = 100;
 
+    var y = document.getElementById("margins");
 
+    y.style.width = (document.anchura-document.margenes-document.margenes)+"pt";
+    y.style.height = (document.altura-document.margenes-document.margenes)+"pt";
+    y.style.opacity = 100;
+
+    var z = document.getElementById("margins2");
+
+    z.style.width = (document.anchura-document.margenes-document.margenes)+"pt";
+    z.style.height = (document.altura-document.margenes-document.margenes)+"pt";
+    z.style.opacity = 100;
+
+    var z2 = document.getElementById("margins3");
+
+    z2.style.width = (document.anchura-document.margenes-document.margenes)+"pt";
+    z2.style.height = (document.altura-document.margenes-document.margenes)+"pt";
+    z2.style.opacity = 100;
+
+    clearBox("margins");
+    clearBox("margins3");
+
+    var divs = parseInt(document.getElementById("divisiones").value);
+    for(var i=0; i<divs+1; i++) {
+        var linea = document.createElement("div");
+        linea.setAttribute("class","lineaEstructura");
+        linea.style.opacity = 100;
+        var margins = document.getElementById("margins");
+        margins.appendChild(linea);
+
+        var linea = document.createElement("div");
+        linea.setAttribute("class","lineaEstructuraV");
+        linea.style.opacity = 100;
+        var margins3 = document.getElementById("margins3");
+        margins3.appendChild(linea);
+    }
+
+    clearBox("margins2");
+
+    var renglones = parseInt(Math.floor(calcLineasActuales()))+1;
+    //console.log("Aparecen: "+ renglones);
+
+    for(var i2=0; i2<renglones; i2++) {
+
+        var renglon = document.createElement("div");
+        renglon.setAttribute("class","renglon");
+        renglon.style.opacity = 100;
+        var margins2 = document.getElementById("margins2");
+        margins2.appendChild(renglon);
+
+        var separacion = document.createElement("div");
+        separacion.setAttribute("class","separacion");
+        separacion.style.height = document.getElementById("interlineado").value+"pt";
+        var margins2 = document.getElementById("margins2");
+        margins2.appendChild(separacion);
+
+        //console.log(i2);
+    }
 }
 
-//document.getElementById("Listobtn").onclick = medidas();
-//document.getElementById("Listobtn").onclick = rectangulo();
+function clearBox(elementID) {
+    var div = document.getElementById(elementID);
+
+    while(div.firstChild) {
+        div.removeChild(div.firstChild);
+    }
+}
